4ffe3c3d362dd33c5ceaf6b2ae431158b9841243
yerloveraaa <yerlovera@gmail.com>
2021-09-12 02:44:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({path: 'variables.env'});
+
+// Cors permite que un cliente se conecta a otro servidor para el intercambio de recursos
+
+const cors = require('cors');
+
+
+
+// conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true
+});
+
+// crear el servidor
+const app = express();
+
+// habilitar bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log(origin)
+        const existe  =  whitelist.some(dominio=> dominio === origin)
+        if(existe){
+            callback(null, true)
+        }else{
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
+
+
+//Habilitar cors
+app.use(cors(corsOptions));
+
+// Rutas de la app
+app.use('/', routes());
+
+// carpeta publica
+app.use(express.static('uploads'));
+
+const host  = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+
+// puerto
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando')
+});
